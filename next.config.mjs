f82955768698fb3f5dec2fe88f2fe0c79c788dd2
Nico Baier <nico.baier@gmail.com>
2026-01-29 10:20:41 -0600

    feat(deps): update dependencies and refactor code for compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	images: {
-		unoptimized: true,
-	},
+	typescript: { ignoreBuildErrors: true },
+	images: { unoptimized: true },
 };
 
 export default nextConfig;
